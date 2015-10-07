8a0781c91139b90af841e2ac84dc79224690cf3d
Marcos Minond <minond.marcos@gmail.com>
2015-10-06 20:31:46 -0600

    tests

--- test/cache_test.ts
+++ test/cache_test.ts
@@ -3,9 +3,29 @@
 'use strict';
 
 import * as assert from 'assert';
-import * as Cache from '../src/cache';
+import {Cache, AsyncStorageCache} from '../src/cache';
 
 describe('Cache', () => {
-    it('#something', () => {
+    let cache, collection;
+
+    beforeEach(() => {
+        collection = {
+            '1': {
+                id: '1',
+                name: 'Marcos'
+            },
+            '2': {
+                id: '2',
+                name: 'Marcos'
+            },
+            '3': {
+                id: '3',
+                name: 'Marcos'
+            }
+        };
+
+        cache = new Cache((id) => collection[id]);
     });
+
+    it('#constructor', () => assert(cache.loader));
 });
